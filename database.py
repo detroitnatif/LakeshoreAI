1dd74f58e4a8689e68c946deea484a60e0af9da4
detroitnatif <tytydizzle124@gmail.com>
2024-04-22 09:24:46 +0200

    builind user db

--- database.py
+++ database.py
@@ -1,18 +1,41 @@
 from deta import Deta
 from dotenv import load_dotenv, find_dotenv
 import os
+import streamlit as st 
+import webbrowser
+import sqlite3
+
 
 load_dotenv(find_dotenv())
 
-deta_key = os.getenv('DETA_API_KEY')
+conn = sqlite3.connect("pwd.db")
+c = conn.cursor()
+c.execute("""CREATE TABLE if not exists pwd_mgr (app_name varchar(20) not null,
+                        user_name varchar(50) not null,
+                        pass_word varchar(50) not null,
+                        email_address varchar(100) not null,
+                        url varchar(255) not null,
+                    primary key(app_name)       
+                    );""")
 
 
-deta = Deta(deta_key)
-db = deta.Base('users_db')
-
-def insert_user(username, name, password):
-    '''Returns the user on a sucessful user creation, otherwise raises an error'''
-    return db.put({'key': username, 'name': name, 'password': password})
-
-insert_user('tklimas', 'ty klimas', '1234')
+def insert_data(u):
+    with conn:
+        c.execute("insert into pwd_mgr values (:app, :user, :pass, :email, :url)", 
+                  {'app': u.app, 'user': u.username, 'pass': u.password, 'email': u.email, 'url': u.url})
+        
+def get_cred_by_app(app):
+    with conn:
+        c.execute("select app_name, user_name, pass_word, email_address, 
+                   url FROM pwd_mgr where app_name = :name;", {'name': app})
+        return c.fetchone()
+    
+def remove_app_cred(app):
+    with conn:
+        c.execute("DELETE from pwd_mgr WHERE app_name = :name", {'name': app})
+        
+def update_password(app,new_pass_word):
+    with conn:
+        c.execute("update pwd_mgr set pass_word = :pass where app_name = :name", 
+                  {'name': app, 'pass': new_pass_word})
 
